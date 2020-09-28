b79968de61d79cc9f9c9b5d66f6561e137205f1c
christinazavou <czavou01@gmail.com>
2020-09-28 16:24:17 +0300

    blender script for multi view render

--- preprocess_buildings/MultiCamerasRender.py
+++ preprocess_buildings/MultiCamerasRender.py
@@ -30,7 +30,6 @@ def initialize():
     #camera.rotation_euler = (0, math.radians(30), math.radians(30))
     camera_constraint = camera.constraints.new('TRACK_TO')
     camera_constraint.target = empty
-    bpy.context.view_layer.objects.active = camera
 
 
 def remove_obj():
@@ -48,11 +47,9 @@ def set_camera_around_point(center_x=0, center_y=0, camera_x=2, camera_y=2, degr
     return (x, y, 0)
 
 
-def render(d=30):
-    camera = bpy.data.objects['Camera']
+def render(camera, d=30):
     x_y_z = set_camera_around_point(degrees=d)
     camera.location = x_y_z
-
     renderer = bpy.context.scene.render # or D.scenes[0].render
     #scale = renderer.resolution_percentage / 100
     #WIDTH = int(renderer.resolution_x * scale)
@@ -70,5 +67,8 @@ if __name__ == '__main__':
     clean_scene()
     initialize()
     load_obj('example_commercial_hotel_mesh0215\COMMERCIALhotel_building_mesh0215.obj')
+    cam = bpy.data.objects['Camera']
+    bpy.context.view_layer.objects.active = cam
+    bpy.context.scene.camera = cam
     for a in [30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360]:
-        render(a)
+        render(cam, a)
