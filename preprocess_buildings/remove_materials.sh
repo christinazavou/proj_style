1c73ae0f95f13c6882e35c6b0cb7530445fefbcb
christinazavou <czavou01@gmail.com>
2020-10-01 13:48:31 +0300

    preprocessing files

---
+++ preprocess_buildings/remove_materials.sh
@@ -0,0 +1,1 @@
+sed '/usemtl/,+1 d' < /media/graphicslab/Elements/ANNFASS_DATA/objs/buildnet_example/COMMERCIALcastle_mesh2985/COMMERCIALcastle_mesh2985.obj > /media/graphicslab/Elements/ANNFASS_DATA/objs/buildnet_example/COMMERCIALcastle_mesh2985/COMMERCIALcastle_mesh2985_nomat.obj
