b8b55208517566432b1c1464ffd9aaffc490d857
christinazavou <czavou01@gmail.com>
2020-10-06 15:39:18 +0300

    buildnet obj to ply with colours

--- preprocess_buildings/buildnetObjToColouredPly.py
+++ preprocess_buildings/buildnetObjToColouredPly.py
@@ -64,16 +64,22 @@ def read_obj(obj_fn):
                 face = [float(line[1].split('/')[0]), float(line[2].split('/')[0]), float(line[3].split('/')[0]),
                         face_normal[0], face_normal[1], face_normal[2]]
                 faces.append(face)
+                edges.append([float(line[1].split('/')[0]), float(line[2].split('/')[0])])
+                edges.append([float(line[2].split('/')[0]), float(line[3].split('/')[0])])
+                edges.append([float(line[3].split('/')[0]), float(line[1].split('/')[0])])
+                edge_colors.append([color, color, color])
 
     vertices = np.vstack(vertices)
     faces = np.vstack(faces)
     face_normals = faces[:, 3:6]
     faces = faces[:, 0:3]
+    edges = np.vstack(edges)
+    edge_colors = np.vstack(edge_colors)
 
-    return vertices, faces.astype(np.int32), face_normals
+    return vertices, faces.astype(np.int32), face_normals, edges, edge_colors
 
 
-def write_ply(ply_fn, vertices, faces, face_normals):
+def write_ply(ply_fn, vertices, faces, face_normals, edges, edge_colors):
     '''Write shape in .ply with face color information
 
        Return:
@@ -84,15 +90,21 @@ def write_ply(ply_fn, vertices, faces, face_normals):
     header = 'ply\n' \
              'format ascii 1.0\n' \
              'element vertex ' + str(len(vertices)) + '\n' \
-                      'property float x\n' \
-                      'property float y\n' \
-                      'property float z\n' \
-                      'element face ' + str(len(faces)) + '\n' \
-                              'property list uchar int vertex_indices\n' \
-                              'property float nx\n' \
-                              'property float ny\n' \
-                              'property float nz\n' \
-                              'end_header\n'
+                  'property float x\n' \
+                  'property float y\n' \
+                  'property float z\n' \
+             'element face ' + str(len(faces)) + '\n' \
+                  'property list uchar int vertex_indices\n' \
+                  'property float nx\n' \
+                  'property float ny\n' \
+                  'property float nz\n' \
+             'element edge ' + str(len(edges)) + '\n' \
+             'property int vertex1\n' \
+             'property int vertex2\n' \
+             'property uchar red\n' \
+             'property uchar green\n' \
+             'property uchar blue\n' \
+             'end_header\n'
 
     if np.min(faces) == 1:
         faces -= 1
@@ -111,6 +123,11 @@ def write_ply(ply_fn, vertices, faces, face_normals):
             row = ' '.join([str(len(face)), str(face[0]), str(face[1]), str(face[2]),
                             str(face_normal[0]), str(face_normal[1]), str(face_normal[2])]) + '\n'
             f_ply.write(row)
+        for edge_ind, edge in enumerate(edges):
+            edge_color = edge_colors[edge_ind]
+            row = ' '.join([str(int(edge[0])), str(int(edge[1])),
+                            str(int(edge_color[0] * 255)), str(int(edge_color[1]*255)), str(int(edge_color[2]*255))]) + '\n'
+            f_ply.write(row)
 
 
 if __name__ == "__main__":
@@ -118,7 +135,7 @@ if __name__ == "__main__":
     filename = "/media/christina/Elements/ANNFASS_DATA/buildnet_objs/objects_with_textures/RESIDENTIALvilla_mesh3265.obj"
 
     # Read obj
-    vertices, faces, face_normals = read_obj(obj_fn=filename)
+    vertices, faces, face_normals, edges, edge_colors = read_obj(obj_fn=filename)
 
     # Re-align faces based on face normals
     if np.min(faces) == 1:
@@ -136,4 +153,4 @@ if __name__ == "__main__":
             faces[angle_ind, 2] = v1
 
     # Write ply
-    write_ply(filename[:-4]+'Marios.ply', vertices, faces, face_normals)
+    write_ply(filename[:-4]+'Marios.ply', vertices, faces, face_normals, edges, edge_colors)
